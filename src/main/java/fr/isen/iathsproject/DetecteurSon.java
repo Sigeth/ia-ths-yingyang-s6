d6e70174bff598209b0918a2a5a9daf39a4ad119
Sigeth <52868615+Sigeth@users.noreply.github.com>
2024-06-07 17:06:36 +0200

    feat: DetecteurSon works now
    thanks to not being stupid with sigmoid haha it's + not * you idiot

--- src/main/java/fr/isen/iathsproject/DetecteurSon.java
+++ src/main/java/fr/isen/iathsproject/DetecteurSon.java
@@ -5,16 +5,14 @@ import fr.isen.FFT.ComplexeCartesien;
 import fr.isen.Son.Son;
 import fr.isen.neurone.*;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 import static fr.isen.FFT.FFTCplx.appliqueSur;
 
 public class DetecteurSon {
     private static final String[] songsname = new String[]{"Bruit", "Carre", "Sinusoide", "Sinusoide2", "Sinusoide3Harmoniques"};
     private static List<Son> sons;
-    private static final int tailleFFT = 512;
+    private static final int tailleFFT = 1024;
 
     public DetecteurSon() {
         sons = new ArrayList<>(List.of());
@@ -24,78 +22,60 @@ public class DetecteurSon {
         }
     }
 
-    private static Neurone trainNeuroneSon(Neurone n, float[] resultats) {
-        for (float synapse : n.synapses())
+    private static float[] signalToModuleFFT(float[] signalData) {
+        Complexe[] signal = new Complexe[tailleFFT];
+        for (int i = 0; i < tailleFFT; ++i)
+            signal[i] = new ComplexeCartesien(signalData[i], 0);
+
+        Complexe[] fft = appliqueSur(signal);
+        float[] entree = new float[tailleFFT];
+        for (int i = 0; i < tailleFFT; ++i) {
+            entree[i] = (float) (Math.sqrt(Math.pow(fft[i].reel(), 2) + Math.pow(fft[i].imag(), 2)));
+        }
+
+        return entree;
+    }
+
+    private static NeuroneSigmoide trainNeuroneFromDataset(float[][] entreesSon, float[] resultats) {
+        NeuroneSigmoide n = new NeuroneSigmoide(tailleFFT);
+
+        /*for (float synapse : n.synapses())
             System.out.print(synapse + " ");
-        System.out.println(" ");
+        System.out.println(" ");*/
 
-        Complexe[][] signals = new Complexe[sons.size()][tailleFFT];
-        Complexe[][] ffts = new Complexe[sons.size()][tailleFFT];
-        float[][] entrees = new float[sons.size()][tailleFFT];
-        for (int j = 0; j < sons.size(); j++) {
-            Complexe[] signal = signals[j];
-            Son s = sons.get(j);
-            for (int i = 0; i < tailleFFT; ++i)
-                signal[i] = new ComplexeCartesien(s.bloc_deTaille(1, tailleFFT)[i], 0);
-
-            ffts[j] = appliqueSur(signal);
-            for (int i = 0; i < tailleFFT; ++i) {
-                float[] entree = entrees[j];
-                Complexe[] fft = ffts[j];
-                entree[i] = (float) (fft[i].reel() + fft[i].imag());
-            }
+        float[][] entrees = new float[entreesSon.length][tailleFFT];
+        for (int j = 0; j < entreesSon.length; j++) {
+            entrees[j] = signalToModuleFFT(entreesSon[j]);
         }
 
         int cptEchecs = n.apprentissage(entrees, resultats);
         System.out.println("Apprentissage terminé avec " + cptEchecs + " échecs");
 
-        for (float synapse : n.synapses())
+        /*for (float synapse : n.synapses())
             System.out.print(synapse + " ");
-        System.out.println(" ");
+        System.out.println(" ");*/
 
         return n;
     }
 
-    private static float testNeuroneSon(Neurone n, float[] resultats) {
+    private static float testNeuroneSon(NeuroneSigmoide n, float[] resultats) {
         int tests = 0;
         int cptEchecs = 0;
+        float[][] entrees = new float[sons.size()][tailleFFT];
+        for (int j = 0; j < sons.size(); j++) {
+            entrees[j] = signalToModuleFFT(sons.get(j).bloc_deTaille(1, tailleFFT));
+        }
 
-        for (float bruit = -128.f; bruit < 128.f; bruit += 1.f) {
-            Complexe[][] signals = new Complexe[sons.size()][tailleFFT];
-            Complexe[][] ffts = new Complexe[sons.size()][tailleFFT];
-            float[][] entrees = new float[sons.size()][tailleFFT];
-            for (int j = 0; j < sons.size(); j++) {
-                Complexe[] signal = signals[j];
-                Son s = sons.get(j);
-                for (int i = 0; i < tailleFFT; ++i)
-                    signal[i] = new ComplexeCartesien(s.bloc_deTaille(1, tailleFFT)[i] + bruit, 0);
-
-                ffts[j] = appliqueSur(signal);
-                for (int i = 0; i < tailleFFT; ++i) {
-                    float[] entree = entrees[j];
-                    Complexe[] fft = ffts[j];
-                    entree[i] = (float) (fft[i].reel() + fft[i].imag());
-                }
-            }
-
+        for (int i=0; i < entrees.length; i++) {
+            tests += 1;
             try {
-                tests += 1;
-                for (int i=0; i < entrees.length; i++) {
-                    float[] entree = entrees[i];
-                    // le neurone calcule sa sortie
-                    n.metAJour(entree);
-                    if (n.getClass() == NeuroneHeaviside.class) {
-                        if (n.sortie() != resultats[i])
-                            throw new AssertionError("Le neurone a renvoyé " + n.sortie() + " au lieu de " + resultats[i]);
-                    } else if (n.getClass() == NeuroneSigmoide.class || n.getClass() == NeuroneReLU.class) {
-                        if (n.sortie() < resultats[i] * 0.5f)
-                            throw new AssertionError("Le neurone a renvoyé " + n.sortie() + " au lieu de " + resultats[i] * 0.5f);
-                    } else if (n.getClass() == NeuroneTanH.class) {
-                        if (resultats[i] == 0 ? n.sortie() > 0 : n.sortie() <= 0)
-                            throw new AssertionError("Le neurone a renvoyé " + n.sortie() + " au lieu de " + resultats[i]);
-                    }
-
-                }
+                float[] entree = entrees[i];
+                // le neurone calcule sa sortie
+                n.metAJour(entree);
+                if (resultats[i] == 1 ? n.sortie() < 0.5f : n.sortie() >= 0.5f)
+                    throw new AssertionError("Le neurone a renvoyé " + n.sortie() + " au lieu de " + resultats[i] * 0.5f);
+                /*else
+                    System.out.println("Le neurone a renvoyé " + n.sortie() + " comme demandé : " + resultats[i] * 0.5f);*/
             } catch (AssertionError e) {
                 // si notre sortie n'est pas celle que l'on attendait, on génére une AssertionError
                 // on incrémente ensuite notre compteur d'échecs et on passe au test suivant
@@ -104,46 +84,119 @@ public class DetecteurSon {
                 // System.out.println("Test non réussi\nRaison : " + e.getMessage());
             }
         }
-
         return 1.f - ((float) cptEchecs / tests);
+    }
+
+    private static float testNeuroneFromDataset(NeuroneSigmoide n, float[][] entreesSon, float[] resultats) {
+        int tests = 0;
+        int cptEchecs = 0;
+
+        float[][] entrees = new float[entreesSon.length][tailleFFT];
+        for (int j = 0; j < entreesSon.length; j++) {
+            entrees[j] = signalToModuleFFT(entreesSon[j]);
+        }
+
+        for (int i=0; i < entrees.length; i++) {
+            tests += 1;
+            try {
+                float[] entree = entrees[i];
+                // le neurone calcule sa sortie
+                n.metAJour(entree);
+                if (resultats[i] == 1 ? n.sortie() < 0.5f : n.sortie() >= 0.5f)
+                    throw new AssertionError("Le neurone a renvoyé " + n.sortie() + " au lieu de " + resultats[i] * 0.5f);
+                /*else
+                    System.out.println("Le neurone a renvoyé " + n.sortie() + " comme demandé : " + resultats[i] * 0.5f);*/
+            } catch (AssertionError e) {
+                // si notre sortie n'est pas celle que l'on attendait, on génére une AssertionError
+                // on incrémente ensuite notre compteur d'échecs et on passe au test suivant
+                cptEchecs += 1;
+                // chaque AssertionError a une raison, que l'on peut afficher grâce au bout de code ci-dessous
+                // System.out.println("Test non réussi\nRaison : " + e.getMessage());
+            }
+        }
+        return 1.f - ((float) cptEchecs / tests);
+    }
+
+    private static float[] normaliseBloc(float[] bloc) {
+        ArrayList<Float> arrayBloc = new ArrayList<>();
+        for (float b : bloc) {
+            arrayBloc.add(Math.abs(b));
+        }
+        float max = Collections.max(arrayBloc);
+
+        for (int i=0; i<bloc.length; i++) {
+            bloc[i] = bloc[i] / max;
+        }
+        return bloc;
     }
 
     public static void main(String[] args) {
 
-        /*System.out.println("Neurone du signal Bruit");
-        NeuroneHeaviside nBruit = (NeuroneHeaviside) trainNeuroneSon(new NeuroneHeaviside(tailleFFT), new float[]{1,0,0,0,0});
-        System.out.println("Taux de réussite : " + Math.round(testNeuroneSon(nBruit, new float[]{1,0,0,0,0}) * 100.f) + "%");
+        Son[] datasetCarre = new Son[200];
+        Son[] datasetSinus = new Son[200];
+        Son[] datasetNoisySinus = new Son[200];
+        Son[] datasetNoisyCarre = new Son[200];
+
+        // Son[] datasetBruit = new Son[200];
+
+        for (int i=0; i < 200; i++) {
+            datasetCarre[i] = new Son("src/main/resources/dataset/carre/" + (i*100+20) + ".wav");
+            datasetSinus[i] = new Son("src/main/resources/dataset/sinus/" + (i*100+20) + ".wav");
+            datasetNoisySinus[i] = new Son("src/main/resources/dataset/noisy_sinus/" + (i*100+20) + ".wav");
+            datasetNoisyCarre[i] = new Son("src/main/resources/dataset/noisy_carre/" + (i*100+20) + ".wav");
+            // datasetBruit[i] = new Son("src/main/resources/dataset/bruit/" + (i*100+20) + ".wav");
+        }
+
+        float[][] entreesDataset = new float[600][tailleFFT];
+        float[] resultatsDatasetCarre = new float[600];
+        float[] resultatsDatasetSinus = new float[600];
+        // float[] resultatsDatasetBruit = new float[600];
+
+        float[][] testNeurone = new float[400][tailleFFT];
+        float[] resultatsTestCarre = new float[400];
+        float[] resultatsTestSinus = new float[400];
+
+
+        for (int i=0; i<400; i++) {
+            if (i < 200) {
+                entreesDataset[i] = normaliseBloc(datasetCarre[i].bloc_deTaille(1, tailleFFT));
+                resultatsDatasetCarre[i] = 1;
+                resultatsDatasetSinus[i] = 0;
+                // resultatsDatasetBruit[i] = 0;
+
+                testNeurone[i] = normaliseBloc(datasetNoisyCarre[i].bloc_deTaille(1, tailleFFT));
+                resultatsTestCarre[i] = 1;
+                resultatsTestSinus[i] = 0;
+            } else if (i < 400){
+                entreesDataset[i] = normaliseBloc(datasetSinus[i-200].bloc_deTaille(1, tailleFFT));
+                resultatsDatasetCarre[i] = 0;
+                resultatsDatasetSinus[i] = 1;
+                // resultatsDatasetBruit[i] = 0;
+
+                testNeurone[i] = normaliseBloc(datasetNoisySinus[i-200].bloc_deTaille(1, tailleFFT));
+                resultatsTestCarre[i] = 0;
+                resultatsTestSinus[i] = 1;
+            } /*else {
+                entreesDataset[i] = datasetBruit[i-400].bloc_deTaille(1, tailleFFT);
+                resultatsDatasetCarre[i] = 0;
+                resultatsDatasetSinus[i] = 0;
+                resultatsDatasetBruit[i] = 1;
+            }*/
+        }
+
+        /*System.out.println("Neurone du bruit");
+        NeuroneSigmoide nBruit = (NeuroneSigmoide) trainNeuroneFromDataset(new NeuroneSigmoide(tailleFFT), entreesDataset, resultatsDatasetBruit);
+        System.out.println("Taux de réussite : " + Math.round(testNeuroneSon(nBruit, new float[]{1,0,0,0,0}) * 100.f) + "%");*/
 
         System.out.println("Neurone du signal Carré");
-        NeuroneHeaviside nCarre = (NeuroneHeaviside) trainNeuroneSon(new NeuroneHeaviside(tailleFFT), new float[]{0,1,0,0,0});
-        System.out.println("Taux de réussite : " + Math.round(testNeuroneSon(nCarre, new float[]{0,1,0,0,0}) * 100.f) + "%");
+        NeuroneSigmoide nCarre = trainNeuroneFromDataset(entreesDataset, resultatsDatasetCarre);
+        System.out.println("Taux de réussite sur l'échantillon proposé : " + Math.round(testNeuroneSon(nCarre, new float[]{0,1,0,0,0}) * 100.f) + "%");
+        System.out.println("Taux de réussite sur le dataset bruité : " + Math.round(testNeuroneFromDataset(nCarre, testNeurone, resultatsTestCarre) * 100.f) + "%");
+
 
         System.out.println("Neurone du signal Sinusoide");
-        NeuroneReLU nSinus = (NeuroneReLU) trainNeuroneSon(new NeuroneReLU(tailleFFT), new float[]{0,0,1,1,1});
-        System.out.println("Taux de réussite : " + Math.round(testNeuroneSon(nSinus, new float[]{0,0,1,1,1}) * 100.f) + "%");*/
-
-
-        for (int i = 0; i < sons.size(); i++) {
-            float[] resultatsTrain = new float[sons.size()];
-            float[] resultatsTest = new float[sons.size()];
-            for (int j=0; j<sons.size(); j++) {
-                resultatsTrain[j] = j == i ? 1 : 0;
-                resultatsTest[j] = j == i ? 1 : 0;
-            }
-
-            if (i == 2) {
-                resultatsTrain = new float[]{0,0,1,1,1};
-                resultatsTest = new float[]{0,0,1,1,1};
-            }
-
-            if (i < 3) {
-                System.out.println(Arrays.toString(resultatsTrain));
-                System.out.println(Arrays.toString(resultatsTest));
-
-                System.out.println("Neurone du signal " + songsname[i]);
-                NeuroneHeaviside n = (NeuroneHeaviside) trainNeuroneSon(new NeuroneHeaviside(tailleFFT), resultatsTrain);
-                System.out.println("Taux de réussite : " + Math.round(testNeuroneSon(n, resultatsTest) * 100.f) + "%");
-            }
-        }
+        NeuroneSigmoide nSinus = trainNeuroneFromDataset(entreesDataset, resultatsDatasetSinus);
+        System.out.println("Taux de réussite sur l'échantillon proposé : " + Math.round(testNeuroneSon(nSinus, new float[]{0,0,1,1,1}) * 100.f) + "%");
+        System.out.println("Taux de réussite sur le dataset bruité : " + Math.round(testNeuroneFromDataset(nSinus, testNeurone, resultatsTestSinus) * 100.f) + "%");
     }
 }
--- src/main/java/fr/isen/neurone/NeuroneSigmoide.java
+++ src/main/java/fr/isen/neurone/NeuroneSigmoide.java
@@ -3,7 +3,7 @@ package fr.isen.neurone;
 public class NeuroneSigmoide extends Neurone
 {
     // Fonction d'activation d'un neurone (peut facilement être modifiée par héritage)
-    protected float activation(final float valeur) {return (float) (1.f / (1.f * Math.exp(0.f-valeur)));}
+    protected float activation(final float valeur) {return (float) (1.f / (1.f + Math.exp(0.f-valeur)));}
 
     // Constructeur
     public NeuroneSigmoide(final int nbEntrees) {super(nbEntrees);}
